2b64e41cb218070bd0cfb2480bd0ac8fb8a0ae98
Keshtel <mahsa.b.k71@gmail.com>
2021-01-13 14:24:10 +0100

    Correcting the DE for gap,chemical synapses and mixed case

--- DynamicsMix.m
+++ DynamicsMix.m
@@ -1,17 +1,18 @@
-function [dV] = Dynamics(~, V, conn, tau, g)
+function [dV] = DynamicsMix(~, V, connGap, connChem, tau, g)
 %dynamics is the differential equation that governs the system
 %V is the vector of membrane potential of different cells. tau is the vector 
 %of time constants for all neurons and g is the measure of nonlinearity 
 %that we first put to 0.
 
+%g = zeros(size(V))';
 dV = zeros(size(V));
-D = zeros(size(conn));
+D = zeros(size(connGap));
 for k=1:length(V)
-    D(k,k) = sum(conn(k,:));
+    D(k,k) = sum(connGap(k,:));
 end
 L=conn-D;
 for i=1:length(V)
-    dV(i) = (L(:,i)'*V-g'*V)./tau(i);%sum over all incoming synapses
+    dV(i) = (connChem(:,i)'*V + L(:,i)'*V-g'*V)./tau(i);
 end    
 end
 
